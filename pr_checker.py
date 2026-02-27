9dafe4834d1a42306440148594f244561361c67f
Filippo Bonazzi <filippo.bonazzi@suse.com>
2026-02-27 12:06:15 +0100

    Format with Black

--- pr_checker.py
+++ pr_checker.py
@@ -11,7 +11,7 @@ import urllib
 
 import requests
 
-DEFAULT_FROM = 'HEAD'
+DEFAULT_FROM = "HEAD"
 
 # Similarity threshold for bug IDs
 SIMILARITY_THRESHOLD = 0.8
@@ -24,17 +24,17 @@ class Bug:
     _mentions: list[str]
     _bugzilla: str
 
-    DEFAULT_BUGZILLA_URL = 'https://bugzilla.suse.com'
+    DEFAULT_BUGZILLA_URL = "https://bugzilla.suse.com"
     MAX_TAG_LENGTH = 16
-    DEFAULT_TAGS = ['bsc', 'boo', 'bnc']
+    DEFAULT_TAGS = ["bsc", "boo", "bnc"]
     MISSING_BUG_STRINGS = (
-        'Missing Bug ID',
-        'You must enter a valid bug number!',
+        "Missing Bug ID",
+        "You must enter a valid bug number!",
     )
     PRIVATE_BUG_STRINGS = (
-        'Bug Access Denied',
-        'You are not authorized to access bug',
-        'To see this bug, you must first',
+        "Bug Access Denied",
+        "You are not authorized to access bug",
+        "To see this bug, you must first",
     )
 
     @classmethod
@@ -54,11 +54,11 @@ class Bug:
     @staticmethod
     def id_to_num(bugid: str) -> str:
         """Convert a bug ID (bsc#123456) to a bug number (123456)."""
-        if '#' in bugid:
-            return bugid.split(sep='#', maxsplit=1)[1]
+        if "#" in bugid:
+            return bugid.split(sep="#", maxsplit=1)[1]
         if bugid.isdigit():
             return bugid
-        raise ValueError(f'Unknown bug ID: {bugid}')
+        raise ValueError(f"Unknown bug ID: {bugid}")
 
     def __init__(self, bugid, mentions=None, bugzilla=None):
         self._id = bugid
@@ -92,7 +92,7 @@ class Bug:
         Returns a tuple (exists, public).
         """
         try:
-            r = requests.get(f'{bugzilla or self.bugzilla}/show_bug.cgi', params={'id': self.num})
+            r = requests.get(f"{bugzilla or self.bugzilla}/show_bug.cgi", params={"id": self.num})
             return (
                 not all(s in r.text for s in self.MISSING_BUG_STRINGS),
                 not all(s in r.text for s in self.PRIVATE_BUG_STRINGS),
@@ -114,19 +114,19 @@ class Commit:
     diff: str
 
     def __str__(self) -> str:
-        return self.commit_id + '\n' + self.message + '\n' + self.diff
+        return self.commit_id + "\n" + self.message + "\n" + self.diff
 
     def lines_added(self) -> list[str]:
         """Return a list of '+' lines in this commit."""
         if self.diff:
-            return '\n'.join(ln for ln in self.diff.splitlines() if (ln.startswith('+') and not ln.startswith('+++')))
+            return "\n".join(ln for ln in self.diff.splitlines() if (ln.startswith("+") and not ln.startswith("+++")))
         else:
             return []
 
     def lines_removed(self) -> list[str]:
         """Return a list of '-' lines in this commit."""
         if self.diff:
-            return '\n'.join(ln for ln in self.diff.splitlines() if (ln.startswith('-') and not ln.startswith('---')))
+            return "\n".join(ln for ln in self.diff.splitlines() if (ln.startswith("-") and not ln.startswith("---")))
         else:
             return []
 
@@ -136,7 +136,7 @@ def validate_url(s: str) -> str:
     # Recognize naked URLs as netlocs (e.g. bugzilla.suse.com)
     if not u.netloc:
         u.netloc = u.path
-    return u._replace(path='', params='', query='', fragment='').geturl()
+    return u._replace(path="", params="", query="", fragment="").geturl()
 
 
 def extract_commit_data(range_revs: list[str], bug_regex: re.Pattern) -> (dict[str, Commit], dict[str, Bug]):
@@ -151,17 +151,16 @@ def extract_commit_data(range_revs: list[str], bug_regex: re.Pattern) -> (dict[s
     for commit in range_revs:
         try:
             # Show git commit message and diff
-            cmd_git_show = ['git', 'show', '-U0', '--format=%B',
-                            '--no-show-signature', commit]
+            cmd_git_show = ["git", "show", "-U0", "--format=%B", "--no-show-signature", commit]
             o = subprocess.run(cmd_git_show, check=True, text=True, capture_output=True)
             # Split commit into message (what comes before 'diff --git ...')
             # and diff (what comes after, including 'diff --git ...')
-            sep = 'diff --git'
+            sep = "diff --git"
             spl = o.stdout.strip().split(sep=sep, maxsplit=1)
             if not spl:
-                print(f'Skipping empty commit {commit}', file=sys.stderr)
+                print(f"Skipping empty commit {commit}", file=sys.stderr)
                 continue
-            c = Commit(commit, '', '')
+            c = Commit(commit, "", "")
             if len(spl) > 0:
                 c.message = spl[0]
             if len(spl) > 1:
@@ -174,12 +173,12 @@ def extract_commit_data(range_revs: list[str], bug_regex: re.Pattern) -> (dict[s
         # Extract bugs from commit message
         for b in set(re.findall(bug_regex, c.message)):
             bugs.setdefault(b, Bug(b))
-            bugs[b].add_mention(f'message:{commit}')
+            bugs[b].add_mention(f"message:{commit}")
 
         # Extract bugs from '+' lines in the commit diff
         for b in set(re.findall(bug_regex, c.lines_added())):
             bugs.setdefault(b, Bug(b))
-            bugs[b].add_mention(f'diff:{commit}')
+            bugs[b].add_mention(f"diff:{commit}")
     return (commits, bugs)
 
 
@@ -200,9 +199,9 @@ def detect_similar_bugs(bugs: dict[str, Bug]) -> int:
             if Bug.id_to_num(b) in e or Bug.id_to_num(e) in b:
                 close_matches.add(e)
         if close_matches:
-            print(f'Warning:\t{b}\t(found in {bugs[b].mentions}) closely matches:')
+            print(f"Warning:\t{b}\t(found in {bugs[b].mentions}) closely matches:")
             for m in close_matches:
-                print(f'\t\t{m}\t(found in {bugs[m].mentions})')
+                print(f"\t\t{m}\t(found in {bugs[m].mentions})")
             print()
             warnings += 1
     return warnings
@@ -220,16 +219,16 @@ def detect_nonexistent_nonpublic_bugs(bugs: dict[str, Bug], bugzilla: str, verbo
     for bugid, bug in bugs.items():
         exists, public = bug.check_status(bugzilla)
         if not exists:
-            print(f'Error:\t\t{bugid}\t(found in {bug.mentions}) does not exist on {bugzilla}!')
+            print(f"Error:\t\t{bugid}\t(found in {bug.mentions}) does not exist on {bugzilla}!")
             errors += 1
         elif verbose:
-            print(f'Debug:\t\t{bugid}\t(found in {bug.mentions}) exists on {bugzilla}')
+            print(f"Debug:\t\t{bugid}\t(found in {bug.mentions}) exists on {bugzilla}")
         if exists:
             if not public:
-                print(f'Warning:\t{bugid}\t(found in {bug.mentions}) is not public on {bugzilla}!')
+                print(f"Warning:\t{bugid}\t(found in {bug.mentions}) is not public on {bugzilla}!")
                 warnings += 1
             elif verbose:
-                print(f'Debug:\t\t{bugid}\t(found in {bug.mentions}) is public on {bugzilla}')
+                print(f"Debug:\t\t{bugid}\t(found in {bug.mentions}) is public on {bugzilla}")
     return (errors, warnings)
 
 
@@ -246,8 +245,8 @@ def detect_removed_bug_refs(bugs: dict[str, Bug], commits: dict[str, Commit], bu
     for commit, c in commits.items():
         for b in set(re.findall(bug_regex, c.lines_removed())):
             # If the bug is not mentioned in any '+' lines, report it as possibly being removed
-            if b not in [x for x in bugs if any(s for s in bugs[x].mentions if s.startswith('diff'))]:
-                print(f'Warning:\t{b}\t is being removed in {commit}')
+            if b not in [x for x in bugs if any(s for s in bugs[x].mentions if s.startswith("diff"))]:
+                print(f"Warning:\t{b}\t is being removed in {commit}")
                 warnings += 1
     return warnings
 
@@ -265,12 +264,12 @@ def detect_bad_hashes(commits: dict[str, Commit]) -> int:
         for ln in c.lines_added():
             # Strip leading '+' and any whitespace
             stripped_ln = ln[1:].strip()
-            if stripped_ln.startswith('hash ='):
+            if stripped_ln.startswith("hash ="):
                 # Extract hash value by stripping usual surrounding characters
-                value = stripped_ln.split(sep='=', maxsplit=1)[1].strip().strip('"\',')
+                value = stripped_ln.split(sep="=", maxsplit=1)[1].strip().strip("\"',")
                 if len(value) != 64:
-                    print(f'Error:\t\tInvalid hash length ({len(value)}, expected 64)')
-                    print(f'\t\tin commit {commit}:')
+                    print(f"Error:\t\tInvalid hash length ({len(value)}, expected 64)")
+                    print(f"\t\tin commit {commit}:")
                     print(ln)
                     errors += 1
     if errors:
@@ -279,63 +278,63 @@ def detect_bad_hashes(commits: dict[str, Commit]) -> int:
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Check git commits for consistency of references and contents')
+    parser = argparse.ArgumentParser(description="Check git commits for consistency of references and contents")
     parser.add_argument(
-        '--from',
+        "--from",
         type=str,
-        dest='git_from',
+        dest="git_from",
         default=DEFAULT_FROM,
         help=f'The source Git object [Default: "{DEFAULT_FROM}"]',
     )
-    limits = parser.add_argument_group('limits', 'Commit range limits (at least one required)')
+    limits = parser.add_argument_group("limits", "Commit range limits (at least one required)")
     limits.add_argument(
-        '--to',
+        "--to",
         type=str,
-        dest='git_to',
+        dest="git_to",
         default=None,
-        help='The target Git object [Default: unset]',
+        help="The target Git object [Default: unset]",
     )
     limits.add_argument(
-        '-n',
-        '--max-count',
+        "-n",
+        "--max-count",
         type=int,
-        help='The number of commits to inspect [Default: not limited]',
+        help="The number of commits to inspect [Default: not limited]",
     )
     parser.add_argument(
-        '-t',
-        '--title',
+        "-t",
+        "--title",
         type=str,
-        help='An additional string to check (PR title)',
+        help="An additional string to check (PR title)",
     )
     parser.add_argument(
-        '-b',
-        '--body',
+        "-b",
+        "--body",
         type=str,
-        help='An additional string to check (PR body)',
+        help="An additional string to check (PR body)",
     )
     parser.add_argument(
-        '--bugzilla',
+        "--bugzilla",
         type=str,
         default=Bug.DEFAULT_BUGZILLA_URL,
         help=f'A custom Bugzilla URL [Default: "{Bug.DEFAULT_BUGZILLA_URL}"]',
     )
     parser.add_argument(
-        '--bug-tag',
+        "--bug-tag",
         type=str,
-        nargs='*',
+        nargs="*",
         default=Bug.DEFAULT_TAGS,
-        help=f'A tag by which to identify bug references (tag#xxx) [Default: {Bug.DEFAULT_TAGS}]',
+        help=f"A tag by which to identify bug references (tag#xxx) [Default: {Bug.DEFAULT_TAGS}]",
     )
     parser.add_argument(
-        '--strict',
-        action='store_true',
-        help='Treat warnings as errors [Default: False]',
+        "--strict",
+        action="store_true",
+        help="Treat warnings as errors [Default: False]",
     )
     parser.add_argument(
-        '-v',
-        '--verbose',
-        action='count',
-        help='Print increasingly verbose information [Up to 2 times, default: 0]',
+        "-v",
+        "--verbose",
+        action="count",
+        help="Print increasingly verbose information [Up to 2 times, default: 0]",
         default=0,
     )
 
@@ -343,7 +342,7 @@ def main():
 
     # At least one of -n or --to must be provided, otherwise this will run on the entire Git history
     if not (args.max_count or args.git_to):
-        print('Invalid commit range: at least one of --to or -n must be specified', file=sys.stderr)
+        print("Invalid commit range: at least one of --to or -n must be specified", file=sys.stderr)
         return 1
 
     # Validate Bugzilla URL
@@ -352,25 +351,25 @@ def main():
     # Validate bug tags
     bug_tags = Bug.validate_tags(args.bug_tag)
     if not bug_tags:
-        print(f'No valid bug tags found (specified {args.bug_tag})', file=sys.stderr)
+        print(f"No valid bug tags found (specified {args.bug_tag})", file=sys.stderr)
         return 1
     bug_regex = re.compile(f'(?:{"|".join(bug_tags)})#\\d+')
 
     # Get list of commits
     try:
-        cmd_git_rev_list = ['git', 'rev-list']
+        cmd_git_rev_list = ["git", "rev-list"]
 
         # Limit commit number if requested
         if args.max_count:
-            cmd_git_rev_list.append(f'-n{args.max_count}')
+            cmd_git_rev_list.append(f"-n{args.max_count}")
 
         # If a destination ref has been provided, select the commit range between the source ref and the merge-base
         # between destination and source (the commits in the source "feature" branch)
         if args.git_to:
-            cmd_git_merge_base = ['git', 'merge-base', args.git_to, args.git_from]
+            cmd_git_merge_base = ["git", "merge-base", args.git_to, args.git_from]
             o = subprocess.run(cmd_git_merge_base, check=True, text=True, capture_output=True)
             merge_base = o.stdout.strip()
-            cmd_git_rev_list.append(f'{merge_base}..{args.git_from}')
+            cmd_git_rev_list.append(f"{merge_base}..{args.git_from}")
         else:
             cmd_git_rev_list.append(args.git_from)
 
@@ -378,8 +377,8 @@ def main():
         o = subprocess.run(cmd_git_rev_list, check=True, text=True, capture_output=True)
         range_revs = o.stdout.strip().splitlines()
         if args.verbose:
-            print(f'Commits in range ({len(range_revs)}):')
-            print('\n'.join(range_revs))
+            print(f"Commits in range ({len(range_revs)}):")
+            print("\n".join(range_revs))
             print()
     except subprocess.CalledProcessError as e:
         print(e, file=sys.stderr)
@@ -391,21 +390,21 @@ def main():
     commits, bugs = extract_commit_data(range_revs, bug_regex)
 
     if args.verbose > 1:
-        print('Commits:')
+        print("Commits:")
         for _, v in commits.items():
             print(v)
             print()
 
     # Extract data from additional fields
-    for text, label in ((args.title, 'PR Title'), (args.body, 'PR Body')):
+    for text, label in ((args.title, "PR Title"), (args.body, "PR Body")):
         if text:
             for b in set(re.findall(bug_regex, text)):
                 bugs.setdefault(b, Bug(b))
                 bugs[b].add_mention(label)
 
     if args.verbose:
-        print(f'Bugs ({len(bugs)}):')
-        print(', '.join(bugs))
+        print(f"Bugs ({len(bugs)}):")
+        print(", ".join(bugs))
         print()
 
     #################################################################
@@ -427,10 +426,10 @@ def main():
     errors += detect_bad_hashes(commits)
 
     if args.verbose:
-        print(f'\nErrors: {errors}\tWarnings: {warnings}')
+        print(f"\nErrors: {errors}\tWarnings: {warnings}")
 
     return errors + warnings if args.strict else errors
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sys.exit(main())
